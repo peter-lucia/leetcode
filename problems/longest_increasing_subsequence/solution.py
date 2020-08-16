62f7aa2af82f91947bd970efe2d55bb0ef461c9d
Peter Lucia <plucia19@gmail.com>
2020-08-16 21:37:22 +0000

    Sync LeetCode submission - Longest Increasing Subsequence (python3)

--- problems/longest_increasing_subsequence/solution.py
+++ problems/longest_increasing_subsequence/solution.py
@@ -8,11 +8,10 @@ class Solution:
         
         L = [0 for _ in range(n)]
         for i in range(0, n):
-            m = 0
+            L[i] = 1
             for j in range(0, i):
-                if nums[i] > nums[j] and L[j] >= m:
-                    m = L[j]
-            L[i] = m + 1
+                if nums[i] > nums[j] and L[i] < 1 + L[j]:
+                    L[i] = 1 + L[j]
             
         return max(L)
             
