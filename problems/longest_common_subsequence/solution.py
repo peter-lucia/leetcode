64640f2b0d26ac11f80e00dca141ea0f79df33bd
Peter Lucia <plucia19@gmail.com>
2020-09-09 05:49:12 +0000

    Sync LeetCode submission - Longest Common Subsequence (python3)

--- problems/longest_common_subsequence/solution.py
+++ problems/longest_common_subsequence/solution.py
@@ -1,18 +1,19 @@
+     
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        if len(text1) == 0 or len(text2) == 0:
+        n = len(text1)
+        m = len(text2)
+        
+        if n == 0 or m == 0:
             return 0
         
-        table = [[0 for _ in range(len(text2)+1)] for _ in range(len(text1)+1)]
-        for i in range(len(text1)):
-            for j in range(len(text2)):
-                if text1[i] == text2[j]:
-                    table[i][j] = 1 + table[i-1][j-1]
+        T = [[0 for _ in range(m+1)] for _ in range(n+1)]
+        
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                if text1[i-1] == text2[j-1]:
+                    T[i][j] = T[i-1][j-1] + 1
                 else:
-                    table[i][j] = max(table[i-1][j], table[i][j-1])
-        max_val = 0
-        for row in table:
-            max_val = max(max(row), max_val)
-        return max_val
-        
-        
+                    T[i][j] = max(T[i-1][j], T[i][j-1])
+                    
+        return T[n][m]
