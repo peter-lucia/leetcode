b68a997c25061549231f63f0b5360e697f0706ef
Peter Lucia <plucia19@gmail.com>
2020-09-09 04:05:58 +0000

    Sync LeetCode submission - Fibonacci Number (python3)

--- problems/fibonacci_number/solution.py
+++ problems/fibonacci_number/solution.py
@@ -1,14 +1,14 @@
 class Solution:
     def fib(self, N: int) -> int:
-        a = 0
-        b = 1
         if N == 0:
-            return a
-        for i in range(1,N):
-            old_a = a
-            a = b
-            b = old_a + b
-        return b
-
-
+            return 0
+        if N == 1:
+            return 1
+        T = [0 for _ in range(N+1)]
+        T[0] = 0
+        T[1] = 1
+        for i in range(2, N+1):
+            T[i] = T[i-1] + T[i-2]
+        return T[N]
+            
         
