6ea740cf069ff7d39498ee6bf692a25816565088
Peter Lucia <plucia19@gmail.com>
2021-03-01 02:46:40 +0000

    Sync LeetCode submission - Two Sum (python3)

--- problems/two_sum/solution.py
+++ problems/two_sum/solution.py
@@ -1,12 +1,20 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+from typing import List
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        lookup = {}
         
+        # use hash table (dictionary) to get linear time complexity
+        tried = {}
         for idx, num in enumerate(nums):
-            if target-num in lookup:
-                return idx, lookup[target-num]
-            
-            lookup[num] = idx
+            neededNum = target - num
+            if neededNum in tried:
+                return [tried[neededNum], idx]
+            tried[num] = idx
+        return []
 
-
-        
+s = Solution()
