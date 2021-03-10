b35ecf7cb3b8fe2631c40669be36658d0cbf8f1b
Peter Lucia <plucia19@gmail.com>
2021-03-10 06:57:17 +0000

    Sync LeetCode submission - Fibonacci Number (cpp)

---
+++ problems/fibonacci_number/solution.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+public:
+    int fib(int n) {
+
+        int a = 0;
+        int b = 1;
+        if (n == 0)
+            return a;
+
+        for (int i = 1; i < n; ++i)
+        {
+            int tmp = b;
+            b += a;
+            a = tmp;
+        }
+        return b;
+    }
+};
