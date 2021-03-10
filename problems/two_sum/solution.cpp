7a9d84a829fc11f3281dd09c0b43ff74728cf78c
Peter Lucia <plucia19@gmail.com>
2021-03-10 06:43:11 +0000

    Sync LeetCode submission - Two Sum (cpp)

--- problems/two_sum/solution.cpp
+++ problems/two_sum/solution.cpp
@@ -1,17 +1,15 @@
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        map<int, int> lookup;
-        vector<int> result;
-        for (int i = 0; i < nums.size(); ++i) {
-            int needed_num = target - nums[i];
-            if (lookup.count(needed_num) > 0) {
-                result.push_back(lookup[needed_num]);
-                result.push_back(i);
-                return result;
-            }
-            lookup.insert(pair(nums[i], i));
+        // maps numbers to their indexes
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            if (map.find(target - nums[i]) != map.end())
+                return {i, map[target-nums[i]]};
+            map[nums[i]] = i;
         }
-        return result;
+        return {-1, -1};
     }
 };
