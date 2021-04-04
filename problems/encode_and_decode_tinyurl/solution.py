ccf8cdfe1a87e0f1fea3ba83fe44828b11b1b5d8
Peter Lucia <plucia19@gmail.com>
2021-04-04 01:47:01 +0000

    Sync LeetCode submission - Encode and Decode TinyURL (python3)

--- problems/encode_and_decode_tinyurl/solution.py
+++ problems/encode_and_decode_tinyurl/solution.py
@@ -1,25 +1,31 @@
+import hashlib
 class Codec:
+    
     def __init__(self):
-        self.lookup = {}
-
+        self.cache = {}
+        
+    def get_md5_hash(self, s):
+        m = hashlib.md5()
+        m.update(s.encode('utf-8'))
+        return m.hexdigest()
+    
     def encode(self, longUrl: str) -> str:
         """Encodes a URL to a shortened URL.
         """
-        alpha_numeric = ["abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"]
-        alpha_numeric_length = len(alpha_numeric)
-        shortened = ""
-        c = len(self.lookup)
-        while c > 0:
-            shortened += alpha_numeric[self.count % alpha_numeric_length]
-            c -= 1
-        self.lookup[shortened] = longUrl
-        return shortened
         
+        letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        
+        result = "www.mysite.com/"
+
+        result += self.get_md5_hash(longUrl)
+        
+        self.cache[result] = longUrl
+        return result     
 
     def decode(self, shortUrl: str) -> str:
         """Decodes a shortened URL to its original URL.
         """
-        return self.lookup.get(shortUrl, None)
+        return self.cache.get(shortUrl, None)
         
 
 # Your Codec object will be instantiated and called as such:
