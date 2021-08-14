e9f6bbc52abcd07edc76614409e208ea7e919d0d
Peter Lucia <plucia19@gmail.com>
2021-08-14 07:29:00 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (python3)

--- problems/letter_combinations_of_a_phone_number/solution.py
+++ problems/letter_combinations_of_a_phone_number/solution.py
@@ -1,34 +1,37 @@
 class Solution:
-
-    def __init__(self):
-        self.lookup = {
-            2: ['a', 'b', 'c'],
-            3: ['d', 'e', 'f'],
-            4: ['g', 'h', 'i'],
-            5: ['j', 'k', 'l'],
-            6: ['m', 'n', 'o'],
-            7: ['p', 'q', 'r', 's'],
-            8: ['t', 'u', 'v'],
-            9: ['w', 'x', 'y', 'z']
-        }
-
+    
+    def get_combos(self, list1, list2, size):
+        result = set()
+        for each in list1:
+            for each2 in combinations(list2, size-1):
+                result.add("{0}{1}".format(each, "".join(each2)))
+        return list(result)
+        
     def letterCombinations(self, digits: str) -> List[str]:
+        lookup = {
+            "1": [],
+            "2":["a", "b", "c"],
+            "3":["d", "e", "f"],
+            "4":["g", "h", "i"],
+            "5":["j", "k", "l"],
+            "6":["m", "n", "o"],
+            "7":["p", "q", "r", "s"],
+            "8":["t", "u", "v"],
+            "9":["w", "x", "y", "z"],
+        }
         
-        """In backtracking, we use recursion to explore all the possibilities
-        until we get the best result for the problem."""
+        if len(digits) == 1:
+            return lookup[digits]
+        elif len(digits) == 2:
+            return self.get_combos(lookup[digits[0]], lookup[digits[1]], 2)
+        elif len(digits) == 3:
+            combos = self.get_combos(lookup[digits[0]], lookup[digits[1]], 2)
+            return self.get_combos(combos, lookup[digits[2]], 2)
+        elif len(digits) == 4:
+            combos = self.get_combos(lookup[digits[0]], lookup[digits[1]], 2)
+            combos = self.get_combos(combos, lookup[digits[2]], 2)
+            return self.get_combos(combos, lookup[digits[3]], 2)
         
-        result = []
-        def backtrack(combinations, remaining_digits):
-            if len(remaining_digits) == 0:
-                result.append(combinations)
-            else:
-                for letter in self.lookup[int(remaining_digits[0])]:
-                    backtrack(combinations + letter, remaining_digits[1:])
+            
         
-        if digits:
-            backtrack("", digits)
-
-        return result
-                    
-
         
