4d0b27db2c3c20a1cb5e840f9334bf98b2ff6721
Peter Lucia <plucia19@gmail.com>
2021-08-21 05:28:12 +0000

    Sync LeetCode submission - Two Sum (typescript)

---
+++ problems/two_sum/solution.ts
@@ -0,0 +1,14 @@
+
+var twoSum = function(nums, target) {
+  let map = new Map();
+  for (let i = 0; i < nums.length; ++i) {
+    let current_num = nums[i]
+    let needed_num = target - current_num;
+    if (map.has(needed_num)) {
+      return [i, map.get(needed_num)];
+    }
+    map.set(current_num, i);
+  }
+  return [-1, -1];
+
+};
