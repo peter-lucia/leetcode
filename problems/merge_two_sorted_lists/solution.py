0f2fd6dbcd2c193557d9044c2bb47f575eb86304
Peter Lucia <plucia19@gmail.com>
2022-01-08 22:27:10 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (python3)

--- problems/merge_two_sorted_lists/solution.py
+++ problems/merge_two_sorted_lists/solution.py
@@ -1,45 +1,44 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def listNodeToArray(self, listNode):
-        result = []
-        if listNode is None:
-            return result
-        while True:
-            result.append(listNode.val)
-            listNode = listNode.next
-            if listNode is None:
-                break
-        return result
-    
-    def arrayToListNode(self, arr):
-        if not arr:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        # Approach
+        # Move list1 head and list2 head through both linked lists
+        # If list11 head is less than list2's head, add list1's head to the result
+        # repeat until we've traversed both lists
+        
+        if not list1 and not list2:
             return None
-        nodes = []
-        node = ListNode(arr[0])
-        nodes.append(node)
-        for num in arr[1:]:
-            node.next = ListNode(num)
-            node = node.next
-            nodes.append(node)
+        if list1 and not list2:
+            return list1
+        if list2 and not list1:
+            return list2
+        if list1.val < list2.val:
+            head = list1
+        else:
+            head = list2 
+        starting_head = [head][0]
+        while list1 or list2:
             
-        result_length = len(nodes)
-        for i in range(0, result_length):
-            if i < result_length - 1:
-                nodes[i].next = nodes[i+1]
-        return nodes[0]
-
-    
-    
-    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        list_one = self.listNodeToArray(l1)
-        list_two = self.listNodeToArray(l2)
-        
-        result = sorted(list_one + list_two, reverse=False)
-        
-        return self.arrayToListNode(result)
+            if list1 and list2:
+                if list1.val < list2.val:
+                    new_node = list1
+                    list1 = list1.next
+                else:
+                    new_node = list2
+                    list2 = list2.next
+            elif list1:
+                new_node = list1
+                list1 = list1.next
+            elif list2:
+                new_node = list2
+                list2 = list2.next
+                
+            head.next = new_node
+            head = head.next
+            
+        return starting_head
         
