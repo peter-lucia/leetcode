36e3e8bbe93a5f122bbe301e19ab73a41e06623a
Peter Lucia <plucia19@gmail.com>
2022-01-13 05:51:15 +0000

    Sync LeetCode submission - Edit Distance (python3)

--- problems/edit_distance/solution.py
+++ problems/edit_distance/solution.py
@@ -1,27 +1,37 @@
 class Solution:
-    def minDistance(self, word1: str, word2: str) -> int:
-        if len(word1) == 0:
-            return len(word2)
-        elif len(word2) == 0:
-            return len(word1)
-        if word1 == word2:
-            return 0
-        n = len(word1)
-        m = len(word2)
-        T = [[0 for _ in range(m+1)] for _ in range(n+1)]
-        
-        for i in range(1, n+1):
-            T[i][0] = i
-        for j in range(1, m+1):
-            T[0][j] = j        
+    def minDistance(self, str1: str, str2: str) -> int:
+        """
+        Time complexity: O(nm)
+        Space compleity: O(nm)
+        """
+        n = len(str1)
+        m = len(str2)
+        dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
 
-        for i in range(1, n+1):
-            for j in range(1, m+1):
-                # word[i-1] or word[j-1] converts 1 based index to 0 based index
-                # and corresponds to diff at T[i][j]
-                diff = 1 if word1[i-1] != word2[j-1] else 0
-                T[i][j] = min(T[i-1][j-1] + diff, T[i][j-1] + 1, T[i-1][j] + 1)
-                
-        return T[n][m]
-        
-        
+        for i in range(n+1):
+            for j in range(m+1):
+
+                # when first string is empty, only option is to insert
+                # all of the characters up to j: requires j operations
+                if i == 0:
+                    dp[i][j] = j
+
+                # if second string is empty, only option is to insert
+                # all of the characters up to i: requires i operations
+                elif j == 0:
+                    dp[i][j] = i
+
+                # if last characters are the same in str1 and str2,
+                # we carry over min operations needed without
+                # including the last characters
+                elif str1[i-1] == str2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+
+                # last two characters are different, add an operation
+                # to the minimal previous number of operations possible
+                # prior to reaching this cell in the dp table
+                else:
+                    dp[i][j] = 1 + min(dp[i][j-1],  # without last char in str2
+                                       dp[i-1][j],  # without last char in str1
+                                       dp[i-1][j-1])  # without either chars in str1 and str2
+        return dp[n][m]
