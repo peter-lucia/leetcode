a1872d5b49ad89065bc20f832ba0e07376472d30
Peter Lucia <plucia19@gmail.com>
2022-01-16 22:25:57 +0000

    Sync LeetCode submission - Longest Common Subsequence (python3)

--- problems/longest_common_subsequence/solution.py
+++ problems/longest_common_subsequence/solution.py
@@ -1,16 +1,35 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        #   ''  a  b  c  d  e
+        # '' 0  0  0  0  0  0
+        # a  0  1  1  1  1  1
+        # e  0  1  1  1  1  2
+        # c  0  1  1  2  2  2
+        # d  0  1  1  1  3  3
+        # e  0  1  1  1  3  4
+
+
+        #   ''  a  b  e  x  q
+        # '' 0  0  0  0  0  0
+        # a  0  1  1  1  1  1
+        # e  0  1  1  2  2  2
+        # c  0  1  1  2  2  2
+        # d  0  1 `1` 2  2  2   # Remember when text1[i] == text2[j], let dp[ii][jj] = dp[ii-1][jj-1] + 1
+        # e  0  1  1  2  2  2
         if len(text1) == 0 or len(text2) == 0:
             return 0
+        n = len(text1)
+        m = len(text2)
 
-        table = [[0 for _ in range(len(text2)+1)] for _ in range(len(text1)+1)]
+        dp = [[0 for _ in range(len(text2)+1)] for _ in range(len(text1)+1)]
         for i in range(len(text1)):
             for j in range(len(text2)):
+                # use i, j for text1 and text2
+                # use ii, jj for dp table
+                ii = i+1
+                jj = j+1
                 if text1[i] == text2[j]:
-                    table[i][j] = 1 + table[i-1][j-1]
+                    dp[ii][jj] = 1 + dp[ii-1][jj-1]
                 else:
-                    table[i][j] = max(table[i-1][j], table[i][j-1])
-        max_val = 0
-        for row in table:
-            max_val = max(max(row), max_val)
-        return max_val
+                    dp[ii][jj] = max(dp[ii-1][jj], dp[ii][jj-1])
+        return dp[n][m]
