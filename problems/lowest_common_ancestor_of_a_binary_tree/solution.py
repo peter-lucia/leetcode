7965de1e0ccb670b118f59610e742b645f8c8d64
Peter Lucia <plucia19@gmail.com>
2022-01-19 00:55:46 +0000

    Sync LeetCode submission - Lowest Common Ancestor of a Binary Tree (python3)

--- problems/lowest_common_ancestor_of_a_binary_tree/solution.py
+++ problems/lowest_common_ancestor_of_a_binary_tree/solution.py
@@ -5,33 +5,37 @@
 #         self.left = None
 #         self.right = None
 
-class Solution:      
-    def __init__(self):
-        self.ans = None
-        
+class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # Use DFS postorder, dfs has two return types, has_p, has_q 
+        # as soon as both are true, return the current node
+                
+        self.lca = None     
+        def dfs(node: TreeNode, has_p, has_q) -> Tuple[bool, bool]:
+            if node is None:
+                return has_p, has_q
+            
+                
+            left_has_p, left_has_q = dfs(node.left, has_p, has_q)
+            right_has_p, right_has_q = dfs(node.right, has_p, has_q)
+            
+            if node.val == p.val:
+                has_p |= 1
+            if node.val == q.val:
+                has_q |= 1
+                
+            has_p |= left_has_p | right_has_p
+            has_q |= left_has_q | right_has_q
+            
+            if has_p == 1 and has_q == 1 and self.lca is None:
+                self.lca = node
+            
+            return has_p, has_q
+            
+        dfs(root, 0, 0)
+        return self.lca
         
-        def recurse_tree(current_node):
-            
-            # if we haven't reached the end of a branch, return false
-            if not current_node:
-                return False
-            
-            # left recursion
-            left_is_p_or_q = recurse_tree(current_node.left)
-            
-            # right recursion
-            right_is_p_or_q = recurse_tree(current_node.right)
-            
-            # if current node is either p or q
-            current_node_is_p_or_q = current_node == p or current_node == q
-            
-            if ((current_node_is_p_or_q and left_is_p_or_q) or 
-                (current_node_is_p_or_q and right_is_p_or_q) or 
-                    (left_is_p_or_q and right_is_p_or_q)):
-                self.ans = current_node
-            
-            return current_node_is_p_or_q or left_is_p_or_q or right_is_p_or_q
-        recurse_tree(root)
-        return self.ans
+        
+        
+        
         
