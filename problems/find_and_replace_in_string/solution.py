60d08889152a3b26495d128df213bcecf5371b62
Peter Lucia <plucia19@gmail.com>
2022-01-19 16:55:57 +0000

    Sync LeetCode submission - Find And Replace in String (python3)

--- problems/find_and_replace_in_string/solution.py
+++ problems/find_and_replace_in_string/solution.py
@@ -1,41 +1,43 @@
 class Solution:
     def findReplaceString(self, s: str, indices: List[int], sources: List[str], targets: List[str]) -> str:
-        # Time complexity: O(nk) where n is the number of sources and k is the max size of a string
-        # Space complexity: O(nk) to house potentially all source and replacement texts
         
-        # Key ideas
         
-        # 1. Create a lookup of mapping an index to a source that was found in s with its replacement text
-        # e.g. for s = "abcd", indices = [0,2], sources = ["ab", "cd"], targets = ["eeee", "bbbb"]
-        # lookup = {
-        #   starting_idx: [source text, replacement text]
-        #     0: ["ab", "eeee"] 
-        #     2: ["cd", "bbbb"] 
-        # }
+        # Approach / Observations
         
-        # 2. Replace the found sources with their targets
-        #    If the current index is not in the lookup, just add the character in s at index to the result
-        #    If the current index is in the lookup, replace its source and increment index by len(source)
+        # Use a lookup to put all valid replacements in
+        # Iterate through the characters in the string, if the index is in the lookup
+        # add the replaced target to the result instead of the original source
+        # increment idx by the length of the source text that was replaced
+        # if the index is not in the lookup, add the character to the result and increment
+        # the idx by one
         
-        letters = defaultdict(str)
-        for idx, each in enumerate(s):
-            letters[idx] = each
+        # Example:
+        # abcd
+        # indices = [0,2]
+        # sources = ["a", "cd"]
+        # targets = ["eee", "ffff"]
         
         lookup = {}
-        for i, source in enumerate(sources):
-            if source == s[indices[i]:indices[i] + len(source)]:
-                lookup[indices[i]] = [source, targets[i]]
         
+        for i in range(len(indices)):
+            start_idx = indices[i]
+            source_word = sources[i]
+            if s[start_idx:start_idx + len(source_word)] == source_word:
+                lookup[start_idx] = (sources[i], targets[i])
+                
         result = ""
-        idx = 0
-        while idx < len(s) or idx in lookup:
-            if idx in lookup:
-                result += lookup[idx][1]
-                idx += len(lookup[idx][0])
+        i = 0
+        while i < len(s):
+            if i in lookup:
+                result += lookup[i][1]
+                i += len(lookup[i][0])  # 
             else:
-                result += s[idx]
-                idx += 1
+                result += s[i]
+                i += 1
         return result
-            
-            
+                
+                
+        
+        
+        
         
