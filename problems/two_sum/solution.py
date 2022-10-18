4b811946a7db3477ed940750e6693619555c5c09
Peter Lucia <plucia19@gmail.com>
2022-10-18 03:10:49 +0000

    Sync LeetCode submission - Two Sum (python3)

--- problems/two_sum/solution.py
+++ problems/two_sum/solution.py
@@ -1,10 +1,14 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+
         lookup = {}
-        for i, num in enumerate(nums):
-            exists_idx = lookup.get(target-num)
-            if exists_idx is not None:
-                return [exists_idx, i]
-            lookup[num] = i
-        return [-1, -1]
+
+        for idx, val in enumerate(nums):
+            num = target - val
+            i = lookup.get(num, None)
+            if i is not None:
+                return [i, idx]
+            lookup[val] = idx
         
+        return [-1, -1]
+            
