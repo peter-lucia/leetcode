28702cb2ab7cc162fbcdd0996d5760992dff2144
Peter Lucia <plucia19@gmail.com>
2023-04-01 18:17:46 +0000

    Sync LeetCode submission - Binary Search (python3)

--- problems/binary_search/solution.py
+++ problems/binary_search/solution.py
@@ -7,10 +7,11 @@ class Solution:
         while low <= high:
             mid = (low + high) // 2
 
-            if nums[mid] > target:
-                high = mid - 1
-            elif nums[mid] < target:
+            if nums[mid] < target:
                 low = mid + 1
+            elif nums[mid] > target:
+                high = mid - 1
             else:
                 return mid
-        return -1 
+        return -1
+
