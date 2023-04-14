7e0a73dc76826d55509de28279977d5597e2b575
Peter Lucia <plucia19@gmail.com>
2023-04-14 06:55:10 +0000

    Sync LeetCode submission - Valid Anagram (python3)

--- problems/valid_anagram/solution.py
+++ problems/valid_anagram/solution.py
@@ -1,3 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        s_dict = defaultdict(int)
+        for k in s:
+            s_dict[k] += 1
+
+        t_dict = defaultdict(int)
+        for k in t:
+            t_dict[k] += 1
+        
+        return s_dict == t_dict
+
