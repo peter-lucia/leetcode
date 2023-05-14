cc7c599aa36028e95e58548a25819fe82c278722
Peter Lucia <plucia19@gmail.com>
2023-05-14 01:17:42 +0000

    Sync LeetCode submission - 3Sum (python3)

--- problems/3sum/solution.py
+++ problems/3sum/solution.py
@@ -1,35 +1,27 @@
 class Solution:
-
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
+
+        nums.sort()
         n = len(nums)
-        if n == 0:
-            return []
-        # O(nlogn)
-        nums = sorted(nums)
-        
-        result = []
 
-        for i in range(n):
-            if nums[i] > 0:
+        results = set()
+        for left in range(n):
+            # Since all numbers after left are larger, if it's > 0, we have to stop
+            if nums[left] > 0:
                 break
-                
-            if i == 0 or nums[i-1] != nums[i]:
-                
-                j, k = i+1, n-1
-                
 
-                while j < k:
-                    _sum = nums[i] + nums[j] + nums[k]
-                    if _sum < 0:
-                        j += 1
-                    elif _sum > 0:
-                        k -= 1
-                    else:
-                        result.append([nums[i], nums[j], nums[k]])
-                        j += 1
-                        k -= 1
-                        while j < k and nums[j-1] == nums[j]:
-                            j += 1
+            middle = left + 1
+            right = n - 1
+            while middle < right and right < n:
+                if nums[left] + nums[middle] + nums[right] < 0: # sum is too small
+                    middle += 1
+                elif nums[left] + nums[middle] + nums[right] > 0: # sum is too large
+                    right -= 1
+                else:
+                    results.add((nums[left], nums[middle], nums[right]))
+                    # only shot we have is by both increasing and decreasing total potential sum
+                    middle += 1 
+                    right -= 1
 
-        return result
+        return list(results)
+
