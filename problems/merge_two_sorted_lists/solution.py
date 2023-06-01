5bd5b6d01749b4135fc7cfb5aa8d2ddd01837a90
Peter Lucia <plucia19@gmail.com>
2023-06-01 03:12:06 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (python3)

--- problems/merge_two_sorted_lists/solution.py
+++ problems/merge_two_sorted_lists/solution.py
@@ -5,40 +5,26 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        # Approach
-        # Move list1 head and list2 head through both linked lists
-        # If list11 head is less than list2's head, add list1's head to the result
-        # repeat until we've traversed both lists
-        
-        if not list1 and not list2:
+        if not (list1 or list2):
             return None
-        if list1 and not list2:
-            return list1
-        if list2 and not list1:
-            return list2
-        if list1.val < list2.val:
-            head = list1
-        else:
-            head = list2 
-        starting_head = [head][0]
+
+        result = ListNode()
+        head = result
         while list1 or list2:
-            
             if list1 and list2:
                 if list1.val < list2.val:
-                    new_node = list1
+                    head.val = list1.val
                     list1 = list1.next
                 else:
-                    new_node = list2
+                    head.val = list2.val
                     list2 = list2.next
             elif list1:
-                new_node = list1
+                head.val = list1.val
                 list1 = list1.next
             elif list2:
-                new_node = list2
+                head.val = list2.val
                 list2 = list2.next
-                
-            head.next = new_node
-            head = head.next
-            
-        return starting_head
-        
+            if list1 or list2:
+                head.next = ListNode()
+                head = head.next
+        return result
