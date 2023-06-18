5cb4de44e5763465f196e2d54e78f8c80df85729
Peter Lucia <plucia19@gmail.com>
2023-06-18 02:59:31 +0000

    Sync LeetCode submission - Top K Frequent Elements (python3)

--- problems/top_k_frequent_elements/solution.py
+++ problems/top_k_frequent_elements/solution.py
@@ -1,12 +1,24 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+      d = defaultdict(int)
+      for n in nums:
+        d[n] += 1
+      
+      # (key, count) -> (-count, key)
+      # negative count to make heap
+      # put the largest element at the top instead of the smallest
+      vals = [(-v, k) for k, v in list(d.items())]
+      heapify(vals)
+      result = []
+      for i in range(k):
+        _, k = heappop(vals)
+        result.append(k)
+      return result
+
+      
         
-        c = Counter(nums)
 
-        vals = sorted([(k, v) for k, v in c.items()], reverse=True, key=lambda val: val[1])
-    
-        return [first for first, second in vals[:k]]
+
 
         
-
-
