c2e091917f0f5a3d71ca8dc28ed5153a9c02c300
Peter Lucia <plucia19@gmail.com>
2023-06-20 05:56:34 +0000

    Sync LeetCode submission - Valid Palindrome (python3)

--- problems/valid_palindrome/solution.py
+++ problems/valid_palindrome/solution.py
@@ -1,30 +1,23 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        """
-        Convert to lower
-        Extract all alphanumeric characters
-        i = 0, j = len(s) - 1
-        Verify each increment of i and
-        each decrement of j points to the same char
-        Stop when i >= j
-        """
-        s = s.lower()
-        valid_chars = set([
-            'a','b','c','d','e','f','g','h',
-            'i','j','k','l','m','n','o','p',
-            'q','r','s','t','u','v','w','x',
-            'y','z','0','1','2','3','4','5',
-            '6','7','8','9',
-        ])
-        p = ""
-        for each in s:
-            if each in valid_chars:
-                p += each
-        i = 0
-        j = len(p) - 1
-        while i < j:
-            if p[i] != p[j]:
-                return False
-            i += 1
-            j -= 1
-        return True
+      # two pointers start 
+      # at beginning and end
+      # move inward as long
+      # as the values are equal
+      i = 0
+      s = s.lower()
+      s = "".join([
+        c for c in s 
+        if c in
+        "abcdefghijklmnopqrstuvwxyz0123456789"
+
+      ])
+      j = len(s) -1
+      while i <= j:
+        if not s[i] == s[j]:
+          return False
+        i +=1
+        j -=1
+      return True
+      
+        
