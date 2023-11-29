888af6593f4b9e8abda9d989421e2c8d9f35f8ce
Peter Lucia <plucia19@gmail.com>
2023-11-29 04:06:36 +0000

    Sync LeetCode submission Runtime - 35 ms (73.95%), Memory - 6.4 MB (16.22%)

--- 0088-merge-sorted-array/solution.go
+++ 0088-merge-sorted-array/solution.go
@@ -1,28 +1,39 @@
 func merge(nums1 []int, m int, nums2 []int, n int)  {
-  i := 0
-  j := 0
-  res := []int{}
-  for i < m && j < n { 
-    if nums1[i] < nums2[j] {
-      res = append(res, nums1[i])
-      i++
-    } else if nums1[i] == nums2[j] {
-      res = append(res, nums1[i], nums2[j])
-      i++
-      j++
-    }  else {
-      res = append(res, nums2[j])
-      j++
+    // copy nums1 into a new array size m
+    // then using the mergesort merge operation, merge the two separate arrays into nums1
+
+    // deep copy the first m elements of nums1 into a new array nums3
+    nums3 := make([]int, m)
+    copy(nums3, nums1)
+
+    i := 0
+    j := 0
+    k := 0
+    // fmt.Printf("src slice: %v, memory address of the backing array: %p\n", nums1, nums1)
+	// fmt.Printf("dst slice: %v, memory address of the backing array: %p\n", nums3, nums3)
+    for (i < m && j < n) {
+        fmt.Println(nums1, nums3, i, nums2, j)
+        if nums3[i] <= nums2[j] {
+            nums1[k] = nums3[i]
+            i++
+            k++
+        } else {
+            nums1[k] = nums2[j]
+            j++
+            k++
+        }
     }
-  }
-  
-  for i < m {
-    res = append(res, nums1[i])
-    i++
-  }
-  for j < n {
-    res = append(res, nums2[j])
-    j++
-  }
-  copy(nums1, res)
+    // fmt.Println("-----------------")
+    // fmt.Println(nums1, nums3, i, nums2, j)
+    for (i < m) {
+        nums1[k] = nums3[i]
+        i++
+        k++
+    }
+    // fmt.Println(nums1, nums3, i, nums2, j)
+    for (j < n) {
+        nums1[k] = nums2[j]
+        j++
+        k++
+    }
 }
