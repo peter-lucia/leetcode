5b6bc44529bb1bd9cbea4f6f6023f567b1d69650
Peter Lucia <plucia19@gmail.com>
2023-12-23 02:57:51 +0000

    Sync LeetCode submission Runtime - 26 ms (61.85%), Memory - 8.7 MB (6.20%)

--- 0189-rotate-array/solution.go
+++ 0189-rotate-array/solution.go
@@ -1,15 +1,24 @@
-func shiftRight(nums []int) {
-  prev := nums[len(nums)-1]
-  i := 0
-  for i < len(nums) {
-      nums[i], prev = prev, nums[i]
-      i++
-  }
-}
+func rotate(nums []int, k int) {
+    // naive solution
+    // []
+    // [1,2,3,4,5,6,7]
 
-func rotate(nums []int, k int)  {
-    for k > 0 {
-        shiftRight(nums)
-        k--
+    // [5], append nums[len(nums) - k + i] for 0 <= i < k
+    // [5], append nums[i] for 0 <= i < n-k
+    // [1,2,3,4,5,6,7]
+    result := []int{}
+    n := len(nums)
+    if k > n {
+        k = k % n
     }
+
+    for i := 0; i < k; i++ {
+        result = append(result, nums[n-k+i])
+    }
+
+    for i := 0; i < n-k; i++ {
+        result = append(result, nums[i])
+    }
+    copy(nums, result)
+    
 }
