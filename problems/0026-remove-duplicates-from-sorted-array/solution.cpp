24e2d84d66cb0836062650582df289179ce8ddf2
Peter Lucia <plucia19@gmail.com>
2024-02-03 05:06:28 +0000

    Sync LeetCode submission Runtime - 7 ms (77.91%), Memory - 21.1 MB (16.18%)

--- problems/0026-remove-duplicates-from-sorted-array/solution.cpp
+++ problems/0026-remove-duplicates-from-sorted-array/solution.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        // keep track of last unique number and current number
-        // if last unique number != curr, increment idx_last_unique
-        // and swap nums[curr] with nums[idx_last_unique]
+        // keep track of first unique element at index, i
+        // keep track of current element at index, j
+        // i is always less than j;
 
-
-        int n = nums.size();
-        int idx_last_unique = 0;
-        int idx_curr = 1;
-
-        while (idx_curr < n) {
-            if (nums[idx_last_unique] == nums[idx_curr]) {
-                idx_curr++;
+        int i = 0;
+        int j = 1;
+        auto n = nums.size();
+        while (j < n) {
+            if (nums[i] == nums[j]) {
+                j++;
             } else {
-                idx_last_unique++;
-                nums[idx_last_unique] = nums[idx_curr];
-                idx_curr++;
+                i++;
+                nums[i] = nums[j];
+                j++;
             }
-            // cout << "[";
-            // for (const int &num: nums) {
-            //     cout << num << " ";
-            // }
-            // cout << "]\n";
-            
         }
-        return ++idx_last_unique;
+        return i+1;
+        
         
     }
 };
